be82a65eb333b5f29659489edd850404f9830457
DESKTOP-ELGSLMU\Fukushiro <jpflc2301@gmail.com>
2022-07-24 23:58:13 -0300

    fix : removido do globalValues

--- src/globalValues.py
+++ src/globalValues.py
@@ -1,5 +1,5 @@
 black_list = []
 owners = ['fukushiro#0449']
-users_to_delete_message = ['Sushikaze#9930']
+users_to_delete_message = []  # 'Sushikaze#9930'
 users_chance_to_delete = []
 # Sushikaze#9930
